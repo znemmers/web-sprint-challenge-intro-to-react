9f92d211d147fd6a9b742bbcf546e68aee2d429c
Zach Nemmers <Zach@Zachs-MacBook-Pro-3.local>
2023-06-14 21:21:35 -0500

    first attempt, not loading names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import SwNames from './components/Character'
+
+const WrapperDiv = styled.div `
+  text-align: center
+  color: black
+  margin: 0
+  padding: 0
+  font-size: 20px
+`
+
+const Sh1 = styled.h1 `
+color: #443e3e;
+text-shadow: 1px 1px 5px #fff;
+`
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        console.log('hello', res.data)
+        
+        setData(res.data)
+      })
+      .catch(err => console.error(err))
+  }, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,9 +36,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Sh1 className="Header">Characters</Sh1>
+      { data && <SwNames star={data} key={data} />}
     </div>
+    
   );
 }
 
 export default App;
+
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react'
+
+const SwNames = (props) => {
+    return (
+        <div className = 'sw-name-wrapper'>
+            <h3>{props.star.name}</h3>
+            <p>{props.star.birth_year}</p>
+            <p>{props.star.gender}</p>
+        </div>
+    )
+}
+export default SwNames;
+
